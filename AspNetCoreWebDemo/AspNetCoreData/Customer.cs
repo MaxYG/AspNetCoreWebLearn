e1e525afb83964e86b3047deb08a3884e3c258b7
TomYangGang <yangg@shinetechchina.com>
2017-12-25 15:05:36 +0800

    1

---
+++ AspNetCoreWebDemo/AspNetCoreData/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreData
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+    }
+}
